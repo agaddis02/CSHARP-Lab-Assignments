352f6dc86e7052ee51c7182bfb68bf4313cf2fd8
Adam Gaddis <agaddis02@live.com>
2019-08-28 14:20:56 -0500

    Lab 1-1A 1st week

---
+++ Lab1-1A/Lab1-1A/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace Lab1_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double area, circumference;
+            Double radius;
+            string str_radius;
+
+            WriteLine("Enter radius");
+            str_radius = ReadLine();
+            radius = Convert.ToInt32(str_radius);
+
+            area = 3.14 * radius * radius;
+            circumference = 3.14 * 2 * radius;
+
+            WriteLine("Area: " + area);
+            WriteLine("Circumference: " + circumference);
+
+
+        }
+    }
+}
---
+++ PracticeProgram/PracticeProgram/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace PracticeProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            WriteLine("Enter x");
+            x = Convert.ToInt32(ReadLine());
+            y = 5;
+            z = x * y - 20;
+            WriteLine("Z is " + z);
+            
+        }
+    }
+}
