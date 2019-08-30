bc98ef4ec7a8e5f6f23bf5084197a3e29c452478
Adam Gaddis <agaddis02@live.com>
2019-08-30 15:04:02 -0500

    Lab1_2A and 1_2B

---
+++ Lab1_2A/Lab1_2A/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab1_2A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Request amount from user and Store in variable "totalAmount"
+            WriteLine("Enter an amount larger than $1");
+            int totalAmount = Convert.ToInt32(ReadLine());
+
+            // Declare Variables
+            int quarters = 25;
+            int dimes = 10;
+            int nickels = 5;
+            int pennies = 1;
+
+            // Calculations
+            int nbrQuarters = (totalAmount / quarters);
+            totalAmount %= quarters;
+            int nbrDimes = (totalAmount / dimes);
+            totalAmount %= dimes;
+            int nbrNickels = (totalAmount / nickels);
+            totalAmount %= nickels;
+            int nbrPennies = (totalAmount / pennies);
+            totalAmount %= pennies;
+
+            // Print Statements
+            WriteLine(nbrQuarters);
+            WriteLine(nbrDimes);
+            WriteLine(nbrNickels);
+            WriteLine(nbrPennies);
+
+        }
+    }
+}
---
+++ Lab1_2B/Lab1_2B/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Lab1_2B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declare Variable
+            Double length, width, height;
+
+            Double door = 8 * 5;
+            Double window = 2 * 3;
+
+            // Prompt user and store values
+            System.Console.WriteLine("Enter length");
+            length = Convert.ToDouble(System.Console.ReadLine());
+
+            System.Console.WriteLine("Enter width");
+            width = Convert.ToDouble(System.Console.ReadLine());
+
+            System.Console.WriteLine("Enter height");
+            height = Convert.ToDouble(System.Console.ReadLine());
+
+
+
+
+
+
+
+            Double surfaceArea = (2 * width * height) + (2 * length * height)  - (door + (2 * window));
+            Double paint = surfaceArea / 120;
+
+            System.Console.WriteLine("The Surface Area is {0}, and the paint need is {1} Gallons", surfaceArea, paint);
+
+
+        }
+    }
+}
