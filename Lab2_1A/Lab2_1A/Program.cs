ed8e94c8f460e197b44638c004abb1110c032826
Adam Gaddis <agaddis02@live.com>
2019-09-04 14:17:18 -0500

    Lab Assignments 2_1A and 2_1B

---
+++ Lab2_1A/Lab2_1A/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+
+namespace Lab2_1A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Adam Gaddis
+             * This program uses formatting in printigng to print out a given table
+             * 
+             * */
+
+            WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////");
+            WriteLine("{0,-8}{1,33}{2,25}", "==", "Student Points", "==");
+            WriteLine("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////////////");
+            WriteLine("{0,-8}{1,20}{2,18}{3,19}", "Name", "Lab", "Bonus", "Total");
+            WriteLine("{0,-8}{1,20}{2,18}{3,19}", "_______", "__", "___", "___");
+            WriteLine("{0,-8}{1,20}{2,18}{3,19}", "\"Joe\"", "43", "7", "50");
+            WriteLine("{0,-8}{1,19}{2,18}{3,19}", "\"William\"", "50", "8", "58");
+            WriteLine("{0,-8}{1,18}{2,18}{3,19}", "\"Mary Sue\"", "39", "10", "49");
+            WriteLine("{0,-8}{1,20}{2,18}{3,19}", "\"Adam\"", "70", "10", "80");
+            WriteLine("{0,-8}{1,20}{2,18}{3,19}", "\"Karen\"", "150", "50", "200");
+
+        }
+
+    }
+}
---
+++ Lab2_1B/Lab2_1B/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+namespace Lab2_1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Adam Gaddis 
+             * This program ask the user for distance and time to calulate average speed
+             * */
+
+
+            // Request input from user for distance and time
+            WriteLine("Enter the Distance");
+            Double distance = Convert.ToDouble(ReadLine());
+
+            WriteLine("Enter the Time");
+            Double time = Convert.ToDouble(ReadLine());
+
+            // Calculation for speed
+            Double speed = distance / time;
+
+
+            // Print out results
+            WriteLine("Distance Covered: " + distance + " miles");
+            WriteLine("Travel Time: " + time + " hours");
+            WriteLine("Average Traveling Speed: " + speed.ToString("F3") + " miles per hour");
+
+
+
+        }
+    }
+}
