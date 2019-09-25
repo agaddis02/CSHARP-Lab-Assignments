8ab4a6f0df3a10ce68037e9aa0d41c944a93b960
Adam Gaddis <agaddis02@live.com>
2019-09-25 11:11:13 -0500

    Trying to optimze Lab5_2C, A and B are completed already

--- Lab5_2C/Lab5_2C/Program.cs
+++ Lab5_2C/Lab5_2C/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace Lab5_2C
 {
@@ -7,31 +8,53 @@ namespace Lab5_2C
         static void Main(string[] args)
         {
             // Declare Variables
-            int num =-1, checker;
+            int num = -1, checker = 0, flag = 0;
 
 
 
-
+            // User doesnt write a number = 0 continue prompting
             while (num != 0)
             {
+                // Prompt and store value
+                WriteLine("Enter the Number to check Prime: ");
+                num = Convert.ToInt32(ReadLine());
 
-                // Prompt user and store value
-                Console.WriteLine("Enter a number: ");
-                num = Convert.ToInt32(Console.ReadLine());
+                // Checked number
+                checker = num / 2;
 
-
-
-                // as long as the number divided by 2 doesnt equal 1 or zero run code
-                for (checker = 2; checker <= num / 2; checker++)
-                 {
-                    // if the number is divisiablel by the checker, its not prime
-                    if (num % checker == 0)
-                     {
-                        Console.WriteLine(num + " is not prime number");
+                // as long as i is less than our given number divided by 2, continue to loop
+                for (int i = 2; i <= checker; i++)
+                {
+                    // if our numbe and i give us a remainder of zero it is not prime
+                    if (num % i == 0)
+                    {
+                        WriteLine("Number is not Prime.");
+                        flag = 1;
+                        break;
                     }
-
                 }
-                Console.WriteLine(num + " is a prime number");
+               
+
+                // 1 and 0 will read as prime although they arent 
+                if (num == 1)
+                {
+                    WriteLine(num + " is not a prime number");
+                }
+                else if(num == 0)
+                {
+                    WriteLine("You have now exitted the loop");
+                }
+                else if (flag == 0)
+                {
+                    WriteLine("Number is Prime.");
+                }
+
+
+
+                
+
+
+
 
             }
         }
