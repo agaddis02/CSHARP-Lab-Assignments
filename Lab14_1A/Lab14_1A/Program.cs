a3a7c19619d564000d0cb906f836ebc3e17e2461
Adam Gaddis <agaddis02@live.com>
2019-09-27 14:19:08 -0500

    Data and file streams

---
+++ Lab14_1A/Lab14_1A/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace Lab14_1A
+{
+    /*
+     * Adam Gaddis
+     * this program checks a .txt file, and does a factorial for each line within the .txt file
+     * */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variable Declaration
+            int i, fact = 1, number;
+
+
+            // Only run code if we find the file
+            if (File.Exists("Lab14A.txt"))
+            {
+                // Create the file and add to file reader
+                FileStream infile = new FileStream("Lab14A.txt", FileMode.Open, FileAccess.Read);
+                StreamReader reader = new StreamReader(infile);
+
+
+                for (i = 1; i <= 5; i++)
+                {
+                    number = Convert.ToInt32(reader.ReadLine());
+                    fact = 1;
+                    for(int j = 1; j <= number; j++)
+                    {
+                        fact = fact * j;
+                    }
+                    WriteLine("the factorial of " + number + "is: " + fact);
+                }
+
+
+                // Close File Stream and Stream Reader
+                reader.Close();
+                infile.Close();
+
+            }
+            else
+            {
+                WriteLine("File was not found");
+            }
+
+
+
+
+
+        }
+    }
+}
---
+++ Lab14_1B/Lab14_1B/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using static System.Console;
+
+namespace Lab14_1B
+{
+    /*
+     * Adam Gaddis
+     * This is program ask a user for the number between one through ten, then outputs the multiplication table
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create instances to Write to a file
+            FileStream outfile = new FileStream("Lab14B.txt", FileMode.Create, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(outfile);
+
+            // Decalre the varuiable
+            int num;
+
+            // Store Value
+            Write("Input the number (Table to be calculated) : ");
+            num = Convert.ToInt32(ReadLine());
+
+            // As long as i is less than or equal to 10 multiple the users number by i 
+            for (int i = 1; i <= 10; i++)
+            {
+
+                writer.WriteLine("" + num + " X " + i + " = "+ num * i);
+                writer.WriteAsync("" + num + " X " + i + " = " + num * i);
+               
+            }
+        }
+    }
+}
