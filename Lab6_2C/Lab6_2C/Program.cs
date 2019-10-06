6b9e4a6ec32d37adf29161930b6e0c070fcbd0ae
agaddis02 <37841766+agaddis02@users.noreply.github.com>
2019-10-06 17:16:21 -0500

    started progress on lab6_2C

---
+++ Lab6_2C/Lab6_2C/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+using static System.Array;
+using System.Linq;
+
+namespace Lab6_2C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constants 
+            const int ROWS = 10;
+            const int COLUMNS = 10;
+            const char DELIM = ',';
+
+            // Declare Variables
+            int[,] numbers = new int[ROWS, COLUMNS];
+            string line;
+
+            // Create our reader 
+            FileStream infile = new FileStream("Lab6_2C.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(infile);
+
+            line = reader.ReadLine();
+
+            for (int i = 0; i < ROWS; i++)
+            {
+                for (int j = 0; j < COLUMNS; i++)
+                {
+                    
+                    numbers[i,j] = line.Split(DELIM)
+                    reader.ReadLine();
+                    WriteLine(numbers[i,j]);
+                }
+            }
+
+        }
+    }
+}
