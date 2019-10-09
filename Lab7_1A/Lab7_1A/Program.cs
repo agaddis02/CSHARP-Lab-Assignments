67cadbd4796a9e908220cc22f3a2409b36deb58e
Adam Gaddis <agaddis02@live.com>
2019-10-09 14:36:32 -0500

    completed labs for 7_1

---
+++ Lab7_1A/Lab7_1A/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using static System.Random;
+using static System.Console;
+
+namespace Lab7_1A
+{
+    /*
+     * Adam Gaddis
+     * This rogram plays a basic game of rock, paper, scissors with the user
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int usersChoice;
+            WriteLine("Enter 0 for Rock, 1 for paper, and 2 for Scissors");
+            usersChoice = int.Parse(ReadLine());
+            determineWinner(programChoice(), usersChoice);
+        }
+
+        public static int programChoice()
+        {
+            int rando;
+            Random random = new Random();
+
+            rando = random.Next(0, 2);
+
+            return rando;
+        }
+
+        public static void determineWinner(int computer, int user)
+        {
+            switch (computer)
+            {
+                case 0:
+                    WriteLine("computer: Rock");
+                    break;
+                case 1:
+                    WriteLine("computer: Paper");
+                    break;
+                case 2:
+                    WriteLine("computer: Scissors");
+                    break;
+                default:
+                   WriteLine("computer: Unknown");
+                    break;
+            }
+
+            switch (user)
+            {
+                case 0:
+                    WriteLine("user: Rock");
+                    break;
+                case 1:
+                    WriteLine("user: Paper");
+                    break;
+                case 2:
+                    WriteLine("user: Scissors");
+                    break;
+                default:
+                    WriteLine("user: Unknown");
+                    break;
+            }
+
+            if (computer == 0 && user == 2)
+            {
+                // Comp wins
+                WriteLine("computer beats user");
+            }
+            else if (computer == 0 && user == 1)
+            {
+                // comp loses
+                WriteLine("computer losess to user");
+            }
+            else if (computer == 0 && user == 0)
+            {
+                // Tie
+                WriteLine("computer ties with user");
+            }
+            else if (user == 0 && computer == 2)
+            {
+                // User wins
+                WriteLine("computer losess to user");
+            }
+            else if (user == 0 && computer == 1)
+            {
+                // User loses
+                WriteLine("computer beats user");
+            }
+            else if (user == 0 && computer == 0)
+            {
+                // Tie
+                WriteLine("computer ties with user");
+            }
+            else if (user == 1 && computer == 1)
+            {
+                // Tie
+                WriteLine("computer ties with user");
+            }
+            else if (user == 2 && computer == 2)
+            {
+                // Tie
+                WriteLine("computer ties with user");
+            }
+            else if (user == 1 && computer == 2)
+            {
+                // computer wins
+                WriteLine("computer beats user");
+            }
+            else if (user == 2 && computer == 1)
+            {
+                // user wins
+                WriteLine("computer losess to user");
+            }
+            else
+            {
+                WriteLine("error");
+            }
+        }
+    }
+}
---
+++ Lab7_1B/Lab7_1B/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+using System.IO;
+
+namespace Lab7_1B
+{
+    /*
+     * Adam Gaddis
+     * This program reads the file and returns the GCD of the pair of numbers 
+     * */
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            // array for inputs
+            int num1, num2;
+            string[] fields;
+            string line;
+
+            // create rader object
+            FileStream infile = new FileStream("Lab7_1B.txt", FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(infile);
+
+            line = reader.ReadLine();
+
+            while (line != null)
+            {
+                // set array fields equal to the file split
+                fields = line.Split(',');
+                num1 = int.Parse(fields[0]);
+                num2 = int.Parse(fields[1]);
+                WriteLine("The GCD of " + num1 + " and " + num2 + " is: " + getDenominator(num1, num2));
+                line = reader.ReadLine();
+            }
+
+
+        }
+
+        public static int getDenominator(int num1, int num2)
+        {
+            while (num1 != num2)
+
+            {
+
+                if (num1 > num2)
+
+                    num1 = num1 - num2;
+
+
+
+                if (num2 > num1)
+
+                    num2 = num2 - num1;
+
+            }
+
+            return num1;
+        }
+
+
+    }
+}
