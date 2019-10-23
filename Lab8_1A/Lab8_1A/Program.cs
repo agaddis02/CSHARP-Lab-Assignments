066d113dd178524c939f6cf46b1aa315d7cc35a5
Adam Gaddis <agaddis02@live.com>
2019-10-23 14:08:24 -0500

    Labs 8_1

---
+++ Lab8_1A/Lab8_1A/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+using System.IO;
+
+namespace Lab8_1A
+{
+    /*
+     * Adam Gaddis
+     * A roll the dice game that has the user continue to try and roll an 11
+     * */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0, di1, di2;
+            Random random = new Random();
+
+            while (sum != 2 && sum != 11)
+            {
+                rollTheDice(out di1, out di2, random);
+                sum = di1 + di2;
+
+                if (sum == 2)
+                {
+                    WriteLine("Snake eyes - you lose :-( ");
+
+                }
+                else if (sum == 11)
+                {
+                    WriteLine("You win!");
+                }
+                else
+                {
+                    WriteLine("Keep rolling");
+                }
+
+                WriteLine("Press Enter to continue rolling");
+                ReadLine();
+                
+            }
+
+        }
+
+        static void rollTheDice(out int di1, out int di2, Random random)
+        {
+            di1 = random.Next(1, 7);
+            printFace(di1);
+            di2 = random.Next(1, 7);
+            printFace(di2);
+
+
+        }
+
+
+
+        static void printFace(int number)
+        {
+            switch (number)
+            {
+                case 1:
+                    WriteLine("******");
+                    WriteLine("* 1 *");
+                    WriteLine("******");
+                    break;
+                case 2:
+                    WriteLine("******");
+                    WriteLine("* 2 *");
+                    WriteLine("******");
+                    break;
+                case 3:
+                    WriteLine("******");
+                    WriteLine("* 3 *");
+                    WriteLine("******");
+                    break;
+                case 4:
+                    WriteLine("******");
+                    WriteLine("* 4 *");
+                    WriteLine("******");
+                    break;
+                case 5:
+                    WriteLine("******");
+                    WriteLine("* 5 *");
+                    WriteLine("******");
+                    break;
+                case 6:
+                    WriteLine("******");
+                    WriteLine("* 6 *");
+                    WriteLine("******");
+                    break;
+            }
+        }
+    }
+}
+
+
+
---
+++ Lab8_1B/Lab8_1B/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+using static System.Array;
+using System.IO;
+
+namespace Lab8_1B
+{
+    /*
+     * Adam Gaddis
+     * This program calculates the volume of a sphere, cube, or cylinder using an overloaded method 
+     */
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Volume Calculator");
+            WriteLine("Input a S for sphere, C for cylinder, and B for box");
+            char choice = char.Parse(ReadLine());
+
+            if (choice == 'S')
+            {
+                WriteLine("Enter Radius");
+                double radius = double.Parse(ReadLine());
+                WriteLine("The Volume is: " + calcVolume(radius));
+            }
+            else if (choice == 'C')
+            {
+                WriteLine("Enter Radius, and height");
+                double radius = double.Parse(ReadLine());
+                double height = double.Parse(ReadLine());
+                WriteLine("The Volume is: " + calcVolume(radius, height));
+            }
+           else  if (choice == 'B')
+            {
+                WriteLine("Enter Length, Width, and height");
+                double length = double.Parse(ReadLine());
+                double width = double.Parse(ReadLine());
+                double height = double.Parse(ReadLine());
+                WriteLine("The Volume is: " + calcVolume(length, width, height));
+            }
+
+        }
+
+
+        // Sphere
+        static double calcVolume(double r)
+        {
+            double volume = 1.333333333333333 * Math.PI * Math.Pow(r, 3);
+            return volume;
+        }
+
+        // Cylinder
+        static double calcVolume(double r, double h)
+        { 
+            double volume = Math.PI * Math.Pow(r,2) * h;
+            return volume;
+        }
+
+        // Box
+        static double calcVolume(double l, double w, double h)
+        {
+            double volume = l * w * h;
+            return volume;
+        }
+    }
+}
